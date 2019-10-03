102bee01742be2d53868784f974f8a62baeee2b8
Masayoshi Kataoka <mkataoka73@gmail.com>
2019-10-03 12:38:38 +0900

    Implement letters method

--- index.js
+++ index.js
@@ -20,7 +20,13 @@ function Phrase(content) {
 
 	// Returns the letters in the content.
 	this.letters = function letters() {
-		return this.content;
+		for (i = 0; i < this.content.length; i++) {
+			let theLetters = [];
+			if ( this.content.charAt(i).match(/[a-zA-Z]/) ) {
+				theLetters.push(this.content.charAt(i));
+			}
+			return theLetters.join("");
+		};
 	}
 
 	// Returns true for a palindrome, false otherwise.
